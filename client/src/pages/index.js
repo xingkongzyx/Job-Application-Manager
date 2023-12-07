5c1493ece0fd37b36695e5f86508e790cedf0a31
zyx@uiuc <zyx97agm@gmail.com>
2023-12-08 01:34:34 +0800

    redeploy

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,5 +2,5 @@ import Landing from "./Landing";
 import Error from "./Error";
 import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
-
+// export
 export { Landing, ProtectedRoute, Error, Register };
